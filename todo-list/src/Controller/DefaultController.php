adacb4cbaaad3cec1d3349c4c9c351c9aec8ca10
rubentrottein <38758157+rubentrottein@users.noreply.github.com>
2025-02-10 19:34:14 +0100

    V1 de l'application OK (local Wamp)

---
+++ todo-list/src/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Form\TaskType;
+use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/')]
+class DefaultController extends AbstractController
+{
+    #[Route('/', name: 'app_route_list', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('home.html.twig');
+    }
+}
